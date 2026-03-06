61c3d26b3307107c0b3b6820bd5a4ef301917973
Admin <Admin@DESKTOP-KU6BLB3.Dlink>
2026-03-06 12:09:12 +0600

    Splash Function for creating the GUI interface part 1

--- src/university_management_system/Splash.java
+++ src/university_management_system/Splash.java
@@ -3,10 +3,10 @@ package university_management_system;
 import javax.swing.*; //used in Java to import all the classes from the Swing package, which is used to create GUI (Graphical User Interface) applications Newer and improved
 import java.awt.*; //Older GUI library 
 
-public class Splash extends JFrame implements Runnable{
+public class Splash extends JFrame implements Runnable {
 
-    
     Thread t;
+
     Splash() {
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/BUBT1.jpg"));
@@ -17,10 +17,19 @@ public class Splash extends JFrame implements Runnable{
 
         t = new Thread(this);
         t.start();
-        
+
         setVisible(true);
 
-      
+        int x = 1;
+        for (int i = 2; i <= 600; i += 6, x += 1) {
+            setLocation(700 - ((i + x) / 2), 400 - (i / 2));
+            setSize(i + 3 * x, i + x / 2);
+
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+            }
+        }
 
         setLocation(350, 150);
         setSize(1300, 750);
@@ -31,5 +40,17 @@ public class Splash extends JFrame implements Runnable{
         Splash s = new Splash();
     }
 
- 
+      public void run() {
+        try {
+            Thread.sleep(4000);
+            setVisible(false);
+            
+            // Next Frame
+          
+        } catch (Exception e) {
+            
+        }
+    }
+
+    
 }
