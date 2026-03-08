4840dcee4a27ddc1d3e1170b5337191392dd67ad
Admin <Admin@DESKTOP-KU6BLB3.Dlink>
2026-03-08 19:48:32 +0600

    Project Menu Bars

--- src/university_management_system/Project.java
+++ src/university_management_system/Project.java
@@ -4,24 +4,78 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class Project extends JFrame {
-    
-     Project() {
-       setSize(1500, 800);
+
+    Project() {
+        setSize(1500, 800);
         setLocation(250, 100);
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/BUBTinside.jpg"));
         Image i2 = i1.getImage().getScaledInstance(1500, 750, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         add(image);
-    
+
+        JMenuBar mb = new JMenuBar();
+
+        
+        // New Information
+        JMenu newInformation = new JMenu("New Information");
+        newInformation.setForeground(Color.BLUE);
+        mb.add(newInformation);
+        
+        JMenuItem facultyInfo = new JMenuItem("New Faculty Information");
+        facultyInfo.setBackground(Color.WHITE);
+        newInformation.add(facultyInfo);
+
+        JMenuItem studentInfo = new JMenuItem("New Student Information");
+        studentInfo.setBackground(Color.WHITE);
+        newInformation.add(studentInfo);
+
+        // View Details
+        JMenu details = new JMenu("View Details");
+        details.setForeground(Color.RED);
+        mb.add(details);
+
+        JMenuItem facultydetails = new JMenuItem("View Faculty Details");
+        facultydetails.setBackground(Color.WHITE);
+        details.add(facultydetails);
+
+        JMenuItem studentdetails = new JMenuItem("View Student Details");
+        studentdetails.setBackground(Color.WHITE);
+        details.add(studentdetails);
+
+        //Apply Leave
+        JMenu leave = new JMenu("Apply Leave");
+        leave.setForeground(Color.BLUE);
+        mb.add(leave);
+
+        JMenuItem facultyleave = new JMenuItem("Faculty Leave");
+        facultyleave.setBackground(Color.WHITE);
+        leave.add(facultyleave);
+
+        JMenuItem studentleave = new JMenuItem("Student Leave");
+        studentleave.setBackground(Color.WHITE);
+        leave.add(studentleave);
+
+        // Leave Details
+        JMenu leaveDetails = new JMenu("Leave Details");
+        leaveDetails.setForeground(Color.RED);
+        mb.add(leaveDetails);
+
+        JMenuItem facultyleavedetails = new JMenuItem("Faculty Leave Details");
+        facultyleavedetails.setBackground(Color.WHITE);
+        leaveDetails.add(facultyleavedetails);
+
+        JMenuItem studentleavedetails = new JMenuItem("Student Leave Details");
+        studentleavedetails.setBackground(Color.WHITE);
+        leaveDetails.add(studentleavedetails);
+
+        setJMenuBar(mb);
+
         setVisible(true);
-     }
-    
-    
-    
-     public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
         new Project();
     }
 }
